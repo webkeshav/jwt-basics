bff38c0fea587a28e62f62cc41a55fc2e4098bad
Keshav <keshavtomar999@gmail.com>
2024-06-05 18:11:30 +0530

    added auth check for dashboard middleware

--- controllers/main.js
+++ controllers/main.js
@@ -20,8 +20,24 @@ const login = async (req,res)=>{
 }
 
 const dashboard = async (req,res)=>{
-    const luckNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:"Hello Keshav",sceret:`Here is your authorized data your lucky number is ${luckNumber}`})
+
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+            throw new CustomAPIError('No token provided',401)
+        }
+
+        const token = authHeader.split(' ')[1]
+        try {
+            const decoded = jwt.verify(token,process.env.JWT_SECRET)
+     
+            const luckNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello ${decoded.username}`,secret:`Here is your authorized data your lucky number is ${luckNumber}`})
+        } catch (error) {
+            throw new CustomAPIError('Not authorized to access this route',401)
+            
+        }
+    
 
 }
 
