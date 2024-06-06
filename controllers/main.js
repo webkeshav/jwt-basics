21c47b4ddfb26164cd8cde28e340f42fbc87a69a
Keshav <keshavtomar999@gmail.com>
2024-06-06 17:52:55 +0530

    Apply auth middleware to dashboard and created special classes for different errors

--- controllers/main.js
+++ controllers/main.js
@@ -21,22 +21,10 @@ const login = async (req,res)=>{
 
 const dashboard = async (req,res)=>{
 
-    const authHeader = req.headers.authorization;
-
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-            throw new CustomAPIError('No token provided',401)
-        }
-
-        const token = authHeader.split(' ')[1]
-        try {
-            const decoded = jwt.verify(token,process.env.JWT_SECRET)
-     
-            const luckNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello ${decoded.username}`,secret:`Here is your authorized data your lucky number is ${luckNumber}`})
-        } catch (error) {
-            throw new CustomAPIError('Not authorized to access this route',401)
-            
-        }
+   console.log(req.user)
+      
+        const luckNumber = Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello ${req.user.username}`,secret:`Here is your authorized data your lucky number is ${luckNumber}`})
     
 
 }
---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnAuthenticatedError = require('./unauthenticated')
+
+module.exports ={
+    CustomAPIError,
+    BadRequestError,
+    UnAuthenticatedError
+}
